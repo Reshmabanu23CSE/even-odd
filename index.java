34dbe149d9ea6cbe0a5b146f03c639a122711604
Reshmabanu23 <36692753+Reshmabanu23CSE@users.noreply.github.com>
2018-08-29 12:12:07 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Evenorodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	     Scanner in = new Scanner(System.in);
+	     System.out.println("enter value");
+	       int a = in.nextInt();
+	       
+	       if(a%2==0)
+	       {
+	       	System.out.println("valid");
+	       }
+	       else
+	       {
+	       	System.out.println("invalid");
+	       }
+	       
+	       
+	       
+	}
+}
